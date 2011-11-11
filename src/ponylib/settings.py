076e9245fd62ac1f7d8d654454d33f60de802fed
Nikita Kovaliov <nikita@maizy.ru>
2011-11-11 18:43:55 +0000

    Fix timezone
    git-svn-id: https://ponylib.googlecode.com/svn/trunk@11 ad6e9521-f130-e9d2-0e1c-83b2c6e7ed82

--- src/ponylib/settings.py
+++ src/ponylib/settings.py
@@ -28,7 +28,7 @@ DATABASE_OPTIONS = {
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'Russia/Moscow'
+TIME_ZONE = 'Europe/Moscow'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
