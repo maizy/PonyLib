89545bd0da2a6157b24944594f557196157e9091
Nikita Kovaliov <nikita@maizy.ru>
2011-11-13 14:56:33 +0400

    Add /var, update .gitignore

--- ponylib/settings.py
+++ ponylib/settings.py
@@ -85,7 +85,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static-deploy')
+STATIC_ROOT = os.path.join(VAR_ROOT, 'static-deploy')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
