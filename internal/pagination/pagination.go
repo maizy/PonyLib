246444b72a2dc49508cc321877b070e0561c7de9
Nikita Kovalev <nikita@maizy.ru>
2021-05-07 18:56:44 +0300

    iss #69: search results pagination

---
+++ internal/pagination/pagination.go
@@ -0,0 +1,54 @@
+package pagination
+
+import (
+	"math"
+
+	"dev.maizy.ru/ponylib/internal/u"
+)
+
+type Pagination struct {
+	NavigationPages []int
+	PreviousPage    int
+	NextPage        int
+}
+
+func CountSlidePagination(size int, currentPage int, perPage int, totalFound int) Pagination {
+	var navigationPages []int = nil
+	var nextPage int
+	var previousPage int
+
+	if totalFound > perPage {
+		lastPage := int(math.Ceil(float64(totalFound) / float64(perPage)))
+		var firstNavPage = 1
+		var lastNavPage = lastPage
+		// 1 2 3 [4] 5 6 7 8
+		if lastPage >= size+1 {
+			half := int(math.Ceil(float64(size-1) / 2))
+			rightWing := half
+			leftWing := size - 1 - rightWing
+
+			// 2 3 4 5 6 7 [8] 9 10 11
+			if currentPage+rightWing > lastPage {
+				rightWing = lastPage - currentPage
+				leftWing = size - 1 - rightWing
+			}
+			if currentPage < half {
+				leftWing = currentPage - 1
+				rightWing = size - 1 - leftWing
+			}
+			firstNavPage = u.IntMax(currentPage-leftWing, 1)
+			lastNavPage = u.IntMin(currentPage+rightWing, lastNavPage)
+		}
+		for i := firstNavPage; i <= lastNavPage; i++ {
+			navigationPages = append(navigationPages, i)
+		}
+		if currentPage < lastPage {
+			nextPage = currentPage + 1
+		}
+		if currentPage > 1 && currentPage <= lastPage {
+			previousPage = currentPage - 1
+		}
+	}
+
+	return Pagination{navigationPages, previousPage, nextPage}
+}
--- ponylib_app/web/books.go
+++ ponylib_app/web/books.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v4/pgxpool"
 
+	"dev.maizy.ru/ponylib/internal/pagination"
 	"dev.maizy.ru/ponylib/internal/u"
 	"dev.maizy.ru/ponylib/ponylib_app/search"
 )
@@ -20,11 +21,14 @@ func BuildBooksHandler(conn *pgxpool.Pool) func(c *gin.Context) {
 		var results *search.BookSearchResult
 		var totalFound int
 
+		// pages count from 1
+		var currentPage int
+		var searchPagination pagination.Pagination
+
 		if query != "" {
-			var offset int
-			offsetQuery := c.DefaultQuery("offset", "0")
-			if offsetI, err := strconv.Atoi(offsetQuery); err == nil {
-				offset = u.IntMax(offsetI, 0)
+			pageQuery := c.DefaultQuery("page", "1")
+			if pageI, err := strconv.Atoi(pageQuery); err == nil {
+				currentPage = u.IntMax(pageI, 1)
 			}
 			limit := 40
 
@@ -37,14 +41,14 @@ func BuildBooksHandler(conn *pgxpool.Pool) func(c *gin.Context) {
 				totalFound = found
 			}
 
-			if totalFound > 0 {
-				if offset >= totalFound {
-					offset = totalFound - 1
-				}
+			offset := (currentPage - 1) * limit
+			searchPagination = pagination.CountSlidePagination(8, currentPage, limit, totalFound)
+
+			if totalFound > 0 && offset < totalFound {
 				if res, err := search.SearchBooks(conn, &queryStruct, offset, limit); err != nil {
 					c.AbortWithError(
 						http.StatusInternalServerError,
-						fmt.Errorf("unable to count matched books: %w", err),
+						fmt.Errorf("unable to get matched books: %w", err),
 					)
 					return
 				} else {
@@ -58,10 +62,12 @@ func BuildBooksHandler(conn *pgxpool.Pool) func(c *gin.Context) {
 			titleQuery = query[:30] + "…"
 		}
 		c.HTML(http.StatusOK, "search.tmpl", WithCommonVars(c, gin.H{
-			"subtitle":   "Books · " + query,
-			"query":      query,
-			"totalFound": totalFound,
-			"results":    results,
+			"subtitle":    "Books · " + query,
+			"query":       query,
+			"totalFound":  totalFound,
+			"results":     results,
+			"currentPage": currentPage,
+			"pagination":  searchPagination,
 		}))
 	}
 }
